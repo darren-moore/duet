342ecae492ed6d50206ccc8919956e6ea6bb4569
Darren Moore <moodarren@gmail.com>
2018-09-15 07:41:38 -0400

    Add remaining files.

--- UbiGame/Source/Game/Util/Note.h
+++ UbiGame/Source/Game/Util/Note.h
@@ -1,1 +1,16 @@
 #pragma once
+
+struct Note{
+	enum eStemType { none, start, middle, end };
+	enum eNoteLength { whole, half, quarter, eighth, sixteenth };
+
+	Note(eStemType stemType, bool isStemUp, bool isDotted, bool isRest) :
+		stemType(stemType), isStemUp(isStemUp), isDotted(isDotted), isRest(isRest) {};
+
+	eNoteLength noteLength;
+	eStemType stemType;
+	bool isStemUp;
+	bool isDotted;
+	bool isRest;
+
+};
